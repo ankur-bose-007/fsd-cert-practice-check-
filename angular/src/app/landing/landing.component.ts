48d1e74fd026a5ad60c96864707d5222a65fbcb6
Ankur Bose <bose.ankur007@gmail.com>
2019-02-12 23:50:40 +0530

    news api added

--- angular/src/app/landing/landing.component.ts
+++ angular/src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GlobalserviceService } from 'src/globalservices/globalservice.service';
+import { NewsService } from '../newsApi/news.service';
 
 @Component({
   selector: 'app-landing',
@@ -8,10 +9,22 @@ import { GlobalserviceService } from 'src/globalservices/globalservice.service';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-  constructor(private router:Router,private globalService:GlobalserviceService) { }
+  mArticles:Array<any>;
+  mSources:Array<any>;
+  constructor(private router:Router,private globalService:GlobalserviceService,private newsapi:NewsService) { }
 
   ngOnInit() {
+    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    //load news sources
+    this.newsapi.initSources().subscribe(data=> this.mSources = data['sources']);  
+    }
+
+
+  searchArticles(source:String){
+    console.log("selected source is: "+source);
+    this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
   }
+
   LogOut(){
     sessionStorage.removeItem('token');
     this.globalService.jwtToken='';
